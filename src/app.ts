ca4a65044c49caaae173f909c982b4d0f5991d0a
sampath88 <sampathreddy8896@gmail.com>
2023-08-22 01:43:03 +0530

    first commit

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+require("dotenv").config();
+import express from "express";
+import config from "config";
+import connectToDb from "./utils/connectToDb";
+import log from "./utils/logger";
+import router from "./routes";
+
+const app = express();
+
+app.use(router)
+
+const port = config.get("port");
+app.listen(port, () => {
+  log.info(`App started at http://lcoalhost:${port}`);
+
+  connectToDb();
+});
